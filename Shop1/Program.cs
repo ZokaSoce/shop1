f49548649251c159db0d2e764e65d54a111610ff
ZokaSoce <zoransoce2@gmail.com>
2022-08-14 16:54:07 +0200

    Add files via upload

--- Shop1/Program.cs
+++ Shop1/Program.cs
@@ -23,7 +23,7 @@ namespace Shop1
                 var service = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
                 service.Database.Migrate();
 
-                service.Database.ExecuteSqlRaw(@"IF NOT EXISTS (select 1 from Users) BEGIN INSERT INTO Users(Email, Password, Role) VALUES('admin@admin.ba', '12345678', 'admin') END IF NOT EXISTS (SELECT 1 FROM Products) BEGIN INSERT INTO Products(Name,Price,Description,Image) VALUES ('Shoei RF-SR Helmet - Solid',1,'Full Face Helmet','/1024x768-f2b21802-64bc-11eb-a115-0242ac130010.webp') INSERT INTO Products(Name,Price,Description,Image) VALUES ('AGV K1 Helmet',2,'Full Face Helmet','agvk1_helmet_750x750.jpg	')  END");
+                service.Database.ExecuteSqlRaw(@"IF NOT EXISTS (select 1 from Users) BEGIN INSERT INTO Users(Email, Password, Role) VALUES('admin@admin.ba', '12345678', 'admin') END IF NOT EXISTS (SELECT 1 FROM Products) BEGIN INSERT INTO Products(Name,Price,Description,Image) VALUES ('Shoei RF-SR Helmet - Solid',1,'Full Face Helmet','/1024x768-f2b21802-64bc-11eb-a115-0242ac130010.webp') INSERT INTO Products(Name,Price,Description,Image) VALUES ('Icon Airflite Rubatone Helmet',2,'Full Face Helmet','icon_airflite_rubatone_helmet_black_750x750.jpg	') INSERT INTO Products(Name,Price,Description,Image) VALUES ('Arai Regent-X Helmet',2,'Full Face Helmet','arai_regent_x_helmet_750x750.jpg	')  END");
             };
             host.Run();
         }
